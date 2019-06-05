715a7a4eb96313e1e0c9150765b94791d4435017
luqingfei <luqingfei@126.com>
2019-06-05 14:57:03 +0800

    aa

---
+++ wp-includes/version.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * WordPress Version
+ * 版本信息
+ *
+ * Contains version information for the current WordPress release.
+ *
+ * @package WordPress
+ * @since 1.1.0
+ */
+
+/**
+ * The WordPress version string
+ *
+ * @global string $wp_version
+ */
+$wp_version = '5.2.1';
+
+/**
+ * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
+ * 支持WordPress DB修订，当对WordPress DB模式进行更改时递增。
+ *
+ * @global int $wp_db_version
+ */
+$wp_db_version = 44719;
+
+/**
+ * Holds the TinyMCE version
+ * 支持TinyMCE版本
+ *
+ * @global string $tinymce_version
+ */
+$tinymce_version = '4940-20190515';
+
+/**
+ * Holds the required PHP version
+ * 支持所需的PHP版本
+ *
+ * @global string $required_php_version
+ */
+$required_php_version = '5.6.20';
+
+/**
+ * Holds the required MySQL version
+ * 支持所需的MySQL版本
+ *
+ * @global string $required_mysql_version
+ */
+$required_mysql_version = '5.0';
